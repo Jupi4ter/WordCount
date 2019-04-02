25619f879d4fef7ac0365a1afc72e72ffbdc6e93
LipeishanDawn <48879020+LipeishanDawn@users.noreply.github.com>
2019-04-02 22:46:01 +0800

    first commit

---
+++ 201731063209/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCounts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CharNum charNum = new CharNum();
+            WordNum wordNum = new WordNum();
+            FileLines fileLine = new FileLines();
+            string filePath = "input.txt";//默认文件为 input.txt
+            int wordLength = 0;
+            int wordCount = 0;
+            string outFile = "";
+            string outPut = "";
+            if (args.Length > 0) // 判断输入参数   
+            {
+                switch (args[0])
+                {
+                    case "-i":
+                        filePath = args[1];
+                        //读入的文件路径
+                        break;
+                    case "-m":
+                        wordLength = int.Parse(args[1]);
+                        //m个单词组成一个词组
+                        break;
+                    case "-n":
+                        wordCount = int.Parse(args[1]);
+                        //输出出现次数最多的前worldCount个单词
+                        break;
+                    case "-o":
+                        outFile = args[1];
+                        //生成的文件路径
+                        break;
+                }
+                if (args.Length > 2)
+                    switch (args[2])
+                    {
+                        case "-i":
+                            filePath = args[3];
+                            break;
+                        case "-m":
+                            wordLength = int.Parse(args[3]);
+                            break;
+                        case "-n":
+                            wordCount = int.Parse(args[3]);
+                            break;
+                        case "-o":
+                            outFile = args[3];
+                            break;
+                    }
+                if (args.Length > 4)
+                    switch (args[4])
+                    {
+                        case "-i":
+                            filePath = args[5];
+                            break;
+                        case "-m":
+                            wordLength = int.Parse(args[5]);
+                            break;
+                        case "-n":
+                            wordCount = int.Parse(args[5]);
+                            break;
+                        case "-o":
+                            outFile = args[5];
+                            break;
+                    }
+                if (args.Length > 6)
+                    switch (args[6])
+                    {
+                        case "-i":
+                            filePath = args[7];
+                            break;
+                        case "-m":
+                            wordLength = int.Parse(args[7]);
+                            break;
+                        case "-n":
+                            wordCount = int.Parse(args[7]);
+                            break;
+                        case "-o":
+                            outFile = args[7];
+                            break;
+                    }
+
+
+                outPut += "characters:" + charNum.getCharCount(filePath) + "\n";
+                outPut += "words:" + wordNum.getWordCount(filePath) + "\n";
+                outPut += "lines:" + fileLine.getFileLineCount(filePath) + "\n";
+
+                wordNum.getWordCount(filePath, wordLength);//先调用方法把单词处理完成   
+                Dictionary<string, int> dict = wordNum.getLinq();//再调用排序 
+
+                int i = 10;
+                if (wordCount != 0)
+                    i = wordCount;
+                foreach (KeyValuePair<string, int> kvp in dict)
+                {
+                    if (i == 0)
+                        break;
+                    else
+                    {
+                        outPut += "<" + kvp.Key + ">:" + kvp.Value + "\n";
+                        i--;
+                    }
+                }
+                if (outFile == "")
+                {
+                    Console.WriteLine(outPut);
+                }
+                else
+                {
+                    FileStream fs = new FileStream(outFile, FileMode.Create);
+                    //获得字节数组
+                    byte[] data = System.Text.Encoding.Default.GetBytes(outPut);
+                    //开始写入
+                    fs.Write(data, 0, data.Length);
+                    //清空缓冲区、关闭流
+                    fs.Flush();
+                    fs.Close();
+                }
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("参数输入错误!");
+            }
+
+
+        }
+    }
+}
---
+++ 201731063209/ConsoleApp1/ConsoleApp1/WordNum.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WordCounts
+{
+    class WordNum
+    {
+        Dictionary<string, int> wordOccNum = new Dictionary<string, int>();
+        public int getWordCount(string FilePath)
+        {
+            int wordCount = 0;
+            String line;
+            StreamReader sr = new StreamReader(FilePath, Encoding.Default);
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] words = line.ToLower().Split(' ');
+                foreach (string str in words)
+                {
+                    char[] cc = str.ToCharArray();
+                    if (cc.Length > 4)
+                    {
+                        if (cc[0] >= 97 && cc[0] <= 122
+                            && cc[1] >= 97 && cc[1] <= 122
+                            && cc[2] >= 97 && cc[2] <= 122
+                            && cc[3] >= 97 && cc[3] <= 122)
+                        {
+
+                            if (wordOccNum.ContainsKey(str))
+                            {
+                                wordOccNum[str]++;
+                            }
+                            else
+                            {
+                                wordOccNum.Add(str, 1);
+                            }
+                            wordCount++;
+
+                        }
+                    }
+                }
+            }
+            sr.Close();
+            return wordCount;
+        }
+
+        //判断和度为 wordLength的词组
+        public void getWordCount(string FilePath, int wordLength)
+        {
+            wordOccNum.Clear();
+            String line;
+            StreamReader sr = new StreamReader(FilePath, Encoding.Default);
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] words = line.ToLower().Split(' ');
+                foreach (string str in words)
+                {
+                    string key = "";
+                    char[] cc = str.ToCharArray();
+                    if (cc.Length > 4
+                        && cc[0] >= 97 && cc[0] <= 122
+                            && cc[1] >= 97 && cc[1] <= 122
+                            && cc[2] >= 97 && cc[2] <= 122
+                            && cc[3] >= 97 && cc[3] <= 122)
+                    {
+                        key += str + " ";
+                        foreach (string str1 in words)
+                        {
+                            char[] cc1 = str1.ToCharArray();
+                            if (cc1.Length > 4
+                                && cc1[0] >= 97 && cc1[0] <= 122
+                                    && cc1[1] >= 97 && cc1[1] <= 122
+                                    && cc1[2] >= 97 && cc1[2] <= 122
+                                    && cc1[3] >= 97 && cc1[3] <= 122)
+                            {
+                                key += str1 + " ";
+                            }
+                            else
+                            {
+                                break;
+                            }
+                            if (key.Split(' ').Length >= 3) break;
+                        }
+                    }
+                    if (key.Split(' ').Length >= 3)
+                    {
+                        if (wordOccNum.ContainsKey(key))
+                        {
+                            wordOccNum[key]++;
+                        }
+                        else
+                        {
+                            wordOccNum.Add(key, 1);
+                        }
+                    }
+                }
+            }
+            sr.Close();
+        }
+
+        public Dictionary<string, int> getLinq()
+        {
+
+            //返回用
+            Dictionary<string, int> returnDict = new Dictionary<string, int>();
+            //按个数降序排列
+            Dictionary<string, int> tempOrder = wordOccNum.OrderByDescending(o => o.Value).ToDictionary(p => p.Key, o => o.Value);
+            //l临时转换用
+            Dictionary<string, int> temp = new Dictionary<string, int>();
+            int lastValue = 0;
+            //循环降序排列的单词列表
+            foreach (KeyValuePair<string, int> kvp in tempOrder)
+            {
+                //只有在个数相同的时候才进行单词名称排序
+                //判断临时用字典表是不是空 ， 空的就把当前人单词和个数放进去
+                if (temp.Count == 0)
+                {
+                    temp.Add(kvp.Key, kvp.Value);
+                    lastValue = kvp.Value;
+                }
+                else if (kvp.Value == lastValue)//不是空 判断是不是和上一个单词的个数相同 相同就放里
+                {
+                    temp.Add(kvp.Key, kvp.Value);
+                }
+                else if (kvp.Value != lastValue)//不是空  也不相同 把列表按照单词名称将序排列，结果放到返回用的字典里
+                {
+                    //把已经有的 放到返回里
+                    temp.OrderByDescending(p => p.Key).ToList().ForEach(x => returnDict.Add(x.Key, x.Value));
+                    temp.Clear();//清空
+                    temp.Add(kvp.Key, kvp.Value);//把当前的放进去
+                    lastValue = kvp.Value;//记录一下这次人单词个数  用于后面判断
+                }
+            }
+            //循环跳出时临时字典里还有  就再处理一下  清空  防止数据忘记处理
+            if (temp.Count > 0)
+            {
+                temp.OrderByDescending(p => p.Key).ToList().ForEach(x => returnDict.Add(x.Key, x.Value));
+            }
+            return returnDict;//排序就结束了
+        }
+    }
+}
