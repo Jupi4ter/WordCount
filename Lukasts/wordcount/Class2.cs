45dceb2a59a1429adfee1aa8f6f3c4f2b90f6923
Lukasts <1628214773@qq.com>
2019-04-03 13:19:06 +0800

    second

---
+++ Lukasts/wordcount/Class2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace ConsoleApp28
+{
+    public class Class2
+    {
+        public static int Countlines(string failname)
+         {
+             using (StreamReader sw = new StreamReader (failname, true))
+             {
+                 int lines = 0;
+                 var text = "";
+                 while ((text = sw.ReadLine() )!= null)
+                 {
+                     lines++;                   
+                 }//计数不为空的行数
+                 return lines;
+             }    
+         }
+       /* public static int Countlines(string text)
+        {
+            int lines = 0;
+            string text1;
+          
+                while ((text1 =!= null)
+                {
+                    lines++;
+                }
+            return lines;
+
+        }*/
+       /*public void Print()
+        {
+            string filename = @"C:\xv\c.txt";
+            using (StreamReader sw = new StreamReader(filename, true))
+            {
+                string text = sw.ReadToEnd();
+                Console.WriteLine("total lines of text:{0}", Countlines(text));
+            }
+        }*/
+       public void Print_lines(string s)
+        {
+           
+            Console.WriteLine("total lines:{0}", Countlines(s));
+        }
+    }
+}
---
+++ Lukasts/wordcount/Class3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace ConsoleApp28
+{
+    class Class3
+    {
+        public static int CharactersNum(string failname)
+        {
+            int Char_Num =0;
+            using (StreamReader sw = new StreamReader(failname, true))
+            {
+                string text = sw.ReadToEnd();
+                char[] everychar = text.ToCharArray();
+                //string pattern = @"^[\u4e00-\u9fa5]+$";
+                //char[] a = pattern.ToCharArray();
+                for (int i = 0; i < everychar.Length; i++)
+                {
+                    if ((int)everychar[i] < 128)
+                    {
+                        Char_Num++;
+                    }//判断非汉字字符总数
+                    //for (int j = 0; j < a.Length; j++)
+                    //{
+                    //}
+                }
+            }
+            return Char_Num;
+        }
+        public void Print_Char_Num(string s)
+        {
+            Console.WriteLine("characters num:{0}", CharactersNum(s));
+        }
+    }
+}
