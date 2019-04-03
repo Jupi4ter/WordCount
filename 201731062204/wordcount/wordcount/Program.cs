7d51da1523c595996ce1e4787ae30eec7c6ed643
umoonn <1464632869@qq.com>
2019-04-03 15:01:03 +0800

    第一次提交(统计字符功能)

---
+++ 201731062204/wordcount/wordcount/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace wordcount
+{
+    class Count
+    {
+        public void countChar(string word)//统计字符个数
+        {
+            int num = 0;
+            char[] word1 = word.ToCharArray();//将接收的英文存入字符数组方便统计个数
+            for (int i = 0; i < word1.Length; i++)
+            {
+                if (word[i] >= 0 && word[i] <= 127)
+                {
+                    num++;
+                }
+            }
+            string result1 = @"F:\WordCount\201731062204\wordcount\wordcount\result.txt";
+            FileStream fs = new FileStream(result1, FileMode.Append);
+            StreamWriter wr = null;
+            wr = new StreamWriter(fs);
+            wr.WriteLine("characters:" + num);
+            wr.Close();
+            fs.Close();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Count count = new Count();//实例化  
+            string word = File.ReadAllText(@"C:\Users\hdkj\Desktop\test.txt").ToUpper();//将输入的英文字符全部转换为大写字符
+            count.countChar(word);
+        }
+    }
+}
