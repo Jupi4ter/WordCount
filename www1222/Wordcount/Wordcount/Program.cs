185261f424462e77bdea22f4c7bd92a94e53e247
www1222 <932899110@qq.com>
2019-04-04 15:46:56 +0800

    First commit

---
+++ www1222/Wordcount/Wordcount/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+
+namespace WordCount
+{
+    class Program
+    {
+
+        Line Line = new Line();
+        static void Main(string[] args)
+        {
+            string filename = Console.ReadLine(); //@"E:\input.txt";
+            Console.WriteLine(Line.GetRows(filename));
+        }
+    }
+}
