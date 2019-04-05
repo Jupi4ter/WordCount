e9651c7fb015b43e25994584e5d6b7ab817bc1c6
trcccc <937510025@qq.com>
2019-04-05 15:23:02 +0800

    first commit

---
+++ trcccc/wordCount/wordCount/ConsoleApp1Tests1/CountCharTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp14;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14.Tests
+{
+    [TestClass()]
+    public class CountCharTests
+    {
+        [TestMethod()]
+        public void CountCharTest()
+        {
+            string text = File.ReadAllText(@"d:\input.txt");
+            //Assert.Fail();
+        }
+    }
+}
---
+++ trcccc/wordCount/wordCount/ConsoleApp1Tests1/CountLineTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp14;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp14.Tests
+{
+    [TestClass()]
+    public class CountLineTests
+    {
+        [TestMethod()]
+        public void CountLineTest()
+        {
+            string text = File.ReadAllText(@"d:\input.txt");
+            CountLine countLine = new CountLine(text);
+            //Assert.Fail();
+        }
+    }
+}
